6de06d55891160a5ee63ce3d6e0d5288764b42c7
PhungKhaiMinh <phungminh808@gmail.com>
2025-03-07 01:10:19 +0700

    Simplify app for testing

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Cài đặt các dependencies cơ bản trước
-pip install --no-cache-dir -r requirements.txt
-
-# Cài đặt các dependencies OCR
-pip install --no-cache-dir -r requirements-ocr.txt 
