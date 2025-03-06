c6f9f6db54c4812c301d38802170534adcbfee84
PhungKhaiMinh <phungminh808@gmail.com>
2025-03-07 00:57:59 +0700

    Update deployment configuration with staged installation

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Cài đặt các dependencies cơ bản trước
+pip install --no-cache-dir -r requirements.txt
+
+# Cài đặt các dependencies OCR
+pip install --no-cache-dir -r requirements-ocr.txt 
