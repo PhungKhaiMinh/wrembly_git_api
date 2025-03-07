17b06d35b85efe460096850e7045cc4f7c8d2e5b
PhungKhaiMinh <phungminh808@gmail.com>
2025-03-07 15:48:05 +0700

    Update deployment configuration

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pip install -r requirements.txt
-python -m uvicorn main:app --host 0.0.0.0 --port 8000 --workers 1 
+python3 -m pip install -r requirements.txt
+python3 -m uvicorn main:app --host 0.0.0.0 --port 8000 --workers 1 
